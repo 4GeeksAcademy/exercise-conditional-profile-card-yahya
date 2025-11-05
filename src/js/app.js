9c287308b23911e8454c8677ca7f083b487be18f
yahyayatusabe <60793191+yahyayatusabe@users.noreply.github.com>
2025-11-05 11:25:23 +0000

    Card arreglada y agregada con Boostrap

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,27 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Pablo"} ${
+    variables.lastName ? variables.lastName : "Motos"
+  }
+          </h1>
+          <h2>${variables.role ? variables.role : "Secretario"}</h2>
+          <h3>${variables.country ? variables.country : "Francia"}, ${
+    variables.city ? variables.city : "Paris"
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : "4geeksacademy"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : "4geeksacademy"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin ? variables.linkedin : "4geeksacademy"
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : "4geeksacademy"
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -56,7 +69,8 @@ window.onload = function() {
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://www.hola.com/horizon/original_aspect_ratio/217fcd299a17-pablo-motos-ig1.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
